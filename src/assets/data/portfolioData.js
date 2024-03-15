e540f320020619f2abead4951ba8bab0aa64ff0c
kyung <bkykim12@gmail.com>
2024-03-15 18:23:00 +0900

    Update

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -43,19 +43,19 @@ const portfolioData = {
       deploy: "https://github.com/bokyung39/White-Rabbit",
     },
     {
-      name: "Rabbit",
-      period: "2022.01 - 2022.02",
+      name: "시워언해 (진행중)",
+      period: "2023.03 ~",
       project: "Web Application",
-      role: "백엔드 담당",
-      techStack: ["Express", "Node.js", "MongoDB", "Mongoose"],
+      role: "풀스텍 담당",
+      techStack: ["React", "Express", "Node.js", "MongoDB"],
       description:
-        "같은 부트캠프를 수강중인 수강생들, 코치님들과 쉽게 소통하고 지식공유를 할 수 있는 커뮤니티를 제작하였습니다!",
+        "서울의 모든 찜질방 정보들을 한 눈에 쉽게 비교하고 예약할 수 있는 서비스입니다.",
       summary:
-        "같은 부트캠프를 수강하는 수강생 및 수료생을 위한 커뮤니티 플랫폼입니다. 같은 목표를 가지고 모인 사람들과 네트워크를 형성할 수 있는 공간이 있었으면 좋겠다는 생각으로 프로젝트를 제작하게 되었습니다.",
-      meaning:
-        "게시판의 기본적인 CRUD부터 mongoose-paginate-v2를 이용한 페이지네이션, 좋아요 기능, 인기 게시물 설정, 검색 기능 등 게시판과 관련된 전반적인 기능들을 모두 담당하여 구현하였고, 배포까지 경험해 본 프로젝트였습니다.\n\n프로젝트 최종 시연 발표 도중 실시간으로 댓글이 200개 이상 달리는 바람에 트래픽이 몰려 서버가 터지는 상황이 발생했었습니다. 당황스러웠지만 실제 운영되고 있는 커뮤니티 서비스를 잠시나마 간접 체험해 보고, 이후 이를 방어할 수 있는 방법에 대해서도 공부하는 계기가 된 값진 경험이었습니다.",
-      thumbnail: "white-rabbit/thumbnail.png",
-      images: ["white-rabbit/signup.png"],
+        "서울의 모든 찜질방 정보들을 한 눈에 쉽게 비교하고 예약할 수 있는 서비스입니다.",
+      meaning: "",
+      thumbnail: "shiwonhae/thumbnail.png",
+      images: ["shiwonhae/thumbnail.png"],
+      github: "https://github.com/JEONGSUJONG/teamproject-shiwonhae",
     },
   ],
 };
--- src/index.js
+++ src/index.js
@@ -26,22 +26,14 @@ import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
 // pages
 import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-// others
 
+// others
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/portfolio" element={<Index />} />
-      <Route path="/nucleo-icons" element={<NucleoIcons />} />
-      <Route path="/landing-page" element={<LandingPage />} />
-      <Route path="/profile-page" element={<ProfilePage />} />
-      <Route path="/register-page" element={<RegisterPage />} />
       <Route path="*" element={<Navigate to="/portfolio" replace />} />
     </Routes>
   </BrowserRouter>
